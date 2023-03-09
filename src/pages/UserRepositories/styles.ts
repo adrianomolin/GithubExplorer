2306f4ea63f0b7a43e7500a1fa9e26d59e13d98b
adrianomolin <adriano.molin@hotmail.com>
2023-03-09 19:33:58 -0300

    feat(scrollbar customization)

--- src/pages/UserRepositories/styles.ts
+++ src/pages/UserRepositories/styles.ts
@@ -6,4 +6,20 @@ export const Container = styled.div`
   overflow: auto;
 
   margin: 0 auto;
+
+  scroll-behavior: smooth;
+  scrollbar-gutter: stable;
+
+  ::-webkit-scrollbar {
+    width: 8px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background-color: transparent;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: #333;
+    border-radius: 50px;
+  }
 `;
